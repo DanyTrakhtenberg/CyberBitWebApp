6514f934876bf6fe34bc0a7495769211706d5d6a
danytrakhtenberg <danytrakhtenberg@gmail.com>
2021-03-21 03:07:20 +0200

    Forgot to save controller before copy

--- CyberBitWebApp/Controllers/UsersController.cs
+++ CyberBitWebApp/Controllers/UsersController.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CyberBitWebApp.Data;
+using CyberBitWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CyberBitWebApp.Controllers
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly TaskContext taskContext;
+
+        public UsersController(TaskContext taskContext)
+        {
+            this.taskContext = taskContext;
+        }
+
         // GET: api/<UsersController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public User Get(string name)
         {
-            return new string[] { "value1", "value2" };
+            var users = this.taskContext.User.Include(e=>e.Tasks).First(e => e.Name == name);
+            return users;
+
         }
 
         // GET api/<UsersController>/5
@@ -28,8 +41,11 @@ namespace CyberBitWebApp.Controllers
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] User user)
         {
+            var userFromDb = taskContext.User.Include(e => e.Tasks).First(e => e.Name == user.Name);
+            userFromDb.Tasks.Add(user.Tasks.First());
+            taskContext.SaveChanges();
         }
 
         // PUT api/<UsersController>/5
