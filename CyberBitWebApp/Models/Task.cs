f4f2679792ac6a0249e300fd406022a2316e03d0
danytrakhtenberg <danytrakhtenberg@gmail.com>
2021-03-26 01:56:28 +0200

    Task in new file

---
+++ CyberBitWebApp/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace CyberBitWebApp.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+    }
+}
